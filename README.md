49e538c7fde0fc7d3c95a6bd6ec02ab5c76935c6
pawkw <peter.weston@execulink.com>
2021-05-10 18:41:38 -0400

    Moved some files over.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tradingbot
-Example trading bot
+
+An example of a trading bot coded in Python. Not suitable for actual trading.
+
+# Do not use for trading
+
+This program is not for use for actual trading.
+
---
+++ main.py
@@ -0,0 +1,37 @@
+import logging
+import tkinter as tk
+from connectors.coinbase_futures import CoinBaseFuturesClient
+import keys
+
+logger = logging.getLogger()
+
+######################
+# Set up logging
+######################
+
+logger.setLevel(logging.DEBUG)
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s %(levelname)s :: %(message)s")
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+######################
+# Main loop
+######################
+
+if __name__ == "__main__":
+
+    root = tk.Tk()
+    client = BinanceFuturesClient(keys.APIKEY, keys.APISECRET, keys.PASSPHRASE, True)
+    data = client.get_balances()
+    data = client.get_historical_data('BTC-USD', '1h')
+    for candle in data:
+        print("Time: %d Open: %f.2 Low: %f.2 High: %f.2 Close: %f.2 Vol: %f.2" % (candle['time'], candle['open'], candle['low'],
+                                                                                  candle['high'], candle['close'], candle['volume']))
+
+    root.mainloop()
