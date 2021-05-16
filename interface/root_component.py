4348a2148c345de5fee737977d07ef8b9b39d963
pawkw <pawkw@users.noreply.github.com>
2021-05-16 14:13:14 -0400

    Finished logging component.

--- interface/root_component.py
+++ interface/root_component.py
@@ -1,5 +1,8 @@
 import tkinter as tk
+import time
+
 from interface.styling import *
+from interface.logging_component import Logging
 
 class Root(tk.Tk):
     def __init__(self):
@@ -8,7 +11,14 @@ class Root(tk.Tk):
         self.title("Trading Bot")
         self.configure(bg=BG_COLOUR)
 
-        self.left_frame = tk.Frame(self, bg=BG_COLOUR)
-        self.left_frame.pack(side=tk.LEFT)
-        self.right_frame = tk.Frame(self, bg=BG_COLOUR)
-        self.right_frame.pack(side=tk.LEFT)  # Yes, on the left.
+        self._left_frame = tk.Frame(self, bg=BG_COLOUR)
+        self._left_frame.pack(side=tk.LEFT)
+        self._right_frame = tk.Frame(self, bg=BG_COLOUR)
+        self._right_frame.pack(side=tk.LEFT)  # Yes, on the left.
+
+        self._logging_frame = Logging(self._left_frame, bg=BG_COLOUR)
+        self._logging_frame.pack(side=tk.TOP)
+
+        self._logging_frame.add_log("This is a test message.")
+        time.sleep(5)
+        self._logging_frame.add_log("Another message.")
--- interface/styling.py
+++ interface/styling.py
@@ -1,2 +1,4 @@
 BG_COLOUR = "gray12"
-FG_COLOUR = "steelblue"
+FG_COLOUR1 = "white"
+FG_COLOUR2 = "SteelBlue1"
+GLOBAL_FONT = ("Calibri", 12, "normal")
